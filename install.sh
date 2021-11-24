af14923ea2dbb5b184ad997b8f86f07978c5cb99
Fabian Späh <fa-sph@web.de>
2021-11-23 19:05:23 -0500

    abc

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ git clone git@github.com:285714/cs655-pw-cracker.git
 cd cs655-pw-cracker
 
 sudo mv cs655.service /etc/systemd/system
+sudo systemctl daemon-reload
 sudo systemctl enable cs655.service
 sudo systemctl restart cs655.service

725c58e8f0f78a7525e98c8fe0215e61856b0723
Fabian Späh <fa-sph@web.de>
2021-11-24 11:52:55 -0500

    abc

--- src/daemon.py
+++ src/daemon.py
@@ -5,7 +5,7 @@ from datetime import datetime
 while True:
     with open("current_time", 'a') as f:
         now = datetime.now()
-        print(now.strftime("%m/%d/%Y, %H:%M:%S"), file=f)
+        print(now.strftime("%d.%m.%Y, %H:%M:%S"), file=f)
         time.sleep(1)
 
 

17505e24d2202c7af0d75bf537bd1ab20c375d15
TD <nguyentaduy@gmail.com>
2021-11-30 15:17:00 -0500

    server

--- src/server/server.py
+++ src/server/server.py
@@ -1,8 +1,12 @@
 import sys
 import socket
 import argparse
+import math
+from multiprocessing import Pool
+from workers import WORKERS
 
 SOCKET_AMOUNT = 100
+MAX_RANGE = 52**5
 
 def read_until_newline(s):
     msg = ""
@@ -11,9 +15,9 @@ def read_until_newline(s):
     assert(msg.index("\n") + 1 == len(msg))
     return msg[:-1]
 
-def connect_worker(hostname, port, x, y, hash, worker_delay=0):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((hostname, port))
+def connect_worker(sock, x, y, hash, worker_delay=0):
+    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # sock.connect((hostname, port))
     output = "Not Found"
     try:
         # sock.settimeout(worker_delay + 1)
@@ -41,15 +45,56 @@ def connect_worker(hostname, port, x, y, hash, worker_delay=0):
         sock.close()
     return output
 
+def parition_job(worker_list, n): # to change
+    assert(len(worker_list) >= n)
+    assigned_workers = []
+    per_worker = int(math.ceil(MAX_RANGE/n))
+    for i in range(n):
+        hostname, port = worker_list[i]
+        x = i*per_worker
+        y = min(MAX_RANGE, (i+1)*per_worker)
+        assigned_workers.append((hostname, port, x, y))
+    return assigned_workers
+
+def map_reduce(worker_list, hash, n):
+    sockets = []
+    processes = []
+    assigned_workers = parition_job(worker_list, n)
+    output = ""
+    pool = Pool()
+    for (hostname, port, x, y) in assigned_workers:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((hostname, port))
+        processes.append(pool.apply_async(connect_worker, (sock, x, y, hash)))
+    while True:
+        for i, f in enumerate(processes):
+            if f.ready() and f.successful() and len(f.get()) == 5:
+                output = f.get()
+                break
+        if output != "" and len(output) == 5:
+            break
+    pool.close()
+    pool.terminate()
+    pool.join()
+    for sock in sockets:
+        try:
+            sock.sendall(str.encode("Stop\n"))
+            sock.close()
+        except Exception as e:
+            print(e)
+        finally:
+            socket.close()
+    return output
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--port', type=int, required=True)
-    parser.add_argument('--hostname', type=str, required=True)
-    parser.add_argument('--x', type=int, required=True)
-    parser.add_argument('--y', type=int, required=True)
+    # parser.add_argument('--port', type=int, required=True)
+    # parser.add_argument('--hostname', type=str, required=True)
+    # parser.add_argument('--x', type=int, required=True)
+    # parser.add_argument('--y', type=int, required=True)
     parser.add_argument('--hash', type=str, required=True)
 
     args = parser.parse_args()
-    output = connect_worker(args.hostname, args.port, args.x, args.y, args.hash)
+    output = map_reduce(WORKERS, args.hash, 2)
     print(output)

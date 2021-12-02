20c708b87cadd97520e5f2686d3fdb27cc734730
nguyentaduy <nguyentaduy@gmail.com>
2021-12-02 02:55:46 -0500

    server

--- src/server/server.py
+++ src/server/server.py
@@ -2,30 +2,78 @@ import sys
 import socket
 import argparse
 import math
-from multiprocessing import Pool
+from multiprocessing import Pool, Manager
 from workers import WORKERS
+import os
 
 SOCKET_AMOUNT = 100
 MAX_RANGE = 52**5
-INIT_POWER = 100.0
-worker_power = []
+MAX_POWER = 100.0
+MIN_POWER = 1.0
+
+worker_power = "./worker_power"
 
 
 def init():
-    for i in range(len(WORKERS)):
-        worker_power.append(INIT_POWER)
+    with open(worker_power, 'w') as f:
+        for i in range(len(WORKERS)):
+            f.write(str(MAX_POWER) + " ")
+
+def listener(q):
+    while True:
+        print("listener")
+        m = q.get()
+        with open(worker_power, 'w') as f:
+            if m == 'kill':
+                break
+            for i in range(len(m)):
+                f.write(str(m[i]) + " ")
+            f.flush()
+
+def update_worker_power(q, w, t):
+    with open(worker_power, 'r') as f:
+        pw = f.readlines()[0].split()
+        pw[w] = str(t(float(pw[w])))
+        print("update", pw, w)
+        q.put(pw)
+
+def get_next_worker():
+    print("get_next_worker")
+    with open(worker_power, 'r') as f:
+        pw = f.readlines()[0].split()
+        workers = [float(i) for i in pw]
+        print(workers)
+        worker_order = [w[0] for w in sorted(enumerate(workers), key=lambda x:x[1])]
+        return worker_order[0]
+def penalty_down(x):
+    return max(MIN_POWER,round(x/2))
+
+def penalty_new_worker(x):
+    return min(MAX_POWER,x-1)
+
+def addback_power(x):
+    return max(MIN_POWER,round(x/2))
 
 def read_until_newline(s):
     try:
-        msg = ""
-        while "\n" not in msg:
-            msg += s.recv(1024).decode()
+        msg = s.recv(1024).decode()
         assert(msg.index("\n") + 1 == len(msg))
         return msg[:-1]
-    except s.timeout as e:
+    except Exception as e:
+        print(e)
         return
 
-def connect_worker(w, sock, x, y, hash, worker_delay=5):
+def assign_new_worker(q, w):
+    if w >= 0 and w < len(WORKERS):
+        update_worker_power(q, w, penalty_down)
+    new_w = get_next_worker()
+    hostname, port = WORKERS[new_w]
+    new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    new_sock.connect((hostname, port))
+    update_worker_power(q, new_w, penalty_new_worker)
+    return new_w, new_sock
+
+def connect_worker(q, w, sock, x, y, hash, worker_delay=3):
     # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # sock.connect((hostname, port))
     output = "Not Found"
@@ -35,12 +83,10 @@ def connect_worker(w, sock, x, y, hash, worker_delay=5):
         sock.sendall(str.encode("Connection\n"))
         result = read_until_newline(sock)
         if result is None:
-            worker_power[w] = max(1, math.round(worker_power[worker_order[i]]/2))
-            new_worker, new_w = partition_job(WORKERS, last_y, y, 1)
-            hostname, port, new_x, new_y = new_worker[0]
-            new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            new_sock.connect((hostname, port))
-            return connect_worker(new_w, new_sock, new_x, new_y, hash, worker_delay)
+            new_w, new_sock = assign_new_worker(q, w)
+            print(w, "down", "assign", new_w)
+            return connect_worker(q, new_w, new_sock, last_y, y, hash, worker_delay)
+
         else:
             assert(result == "200 OK: Ready")
 
@@ -49,12 +95,9 @@ def connect_worker(w, sock, x, y, hash, worker_delay=5):
         while True:
             msg = read_until_newline(sock)
             if msg is None:
-                worker_power[w] = max(1, math.round(worker_power[worker_order[i]]/2))
-                new_worker, new_w = partition_job(WORKERS, last_y, y, 1)
-                hostname, port, new_x, new_y = new_worker[0]
-                new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                new_sock.connect((hostname, port))
-                return connect_worker(new_w, new_sock, new_x, new_y, hash, worker_delay)
+                new_w, new_sock = assign_new_worker(q, w)
+                print(w, "down", "assign", new_w)
+                return connect_worker(q, new_w, new_sock, last_y, y, hash, worker_delay)
             elif len(msg) == 5:
                 output = msg
                 break
@@ -65,46 +108,50 @@ def connect_worker(w, sock, x, y, hash, worker_delay=5):
             else:
                 break
         sock.sendall(str.encode("Stop\n"))
-        worker_power[w] = min(INIT_POWER, worker_power[w]+1)
+        update_worker_power(q, w, addback_power)
         sock.close()
     except Exception as e:
         print(e)
         if last_y < y:
-            worker_power[w] = max(1, math.round(worker_power[worker_order[i]]/2))
-            new_worker, new_w = partition_job(WORKERS, last_y, y, 1)
-            hostname, port, new_x, new_y = new_worker[0]
-            new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            new_sock.connect((hostname, port))
-            return connect_worker(new_w, new_sock, new_x, new_y, hash, worker_delay)
+            new_w, new_sock = assign_new_worker(q, w)
+            print(w, "down", "assign", new_w)
+            return connect_worker(q, new_w, new_sock, last_y, y, hash, worker_delay)
     finally:
         sock.close()
     return output
 
-def partition_job(worker_list, start, end, n): # to change
-    assert(len(worker_list) >= n)
-    worker_order = [w[0] for w in sorted(enumerate(worker_power), key=lambda x:x[1])]
+def partition_job(q, start, end, n): # to change
+    assert(n <= len(WORKERS))
     assigned_workers = []
     per_worker = int(math.ceil((end-start)/n))
+
+    with open(worker_power, 'r') as f:
+        pw = f.readlines()[0].split()
+        workers = [float(i) for i in pw]
+        worker_order = [w[0] for w in sorted(enumerate(workers), key=lambda x:x[1])]
+
     for i in range(n):
-        hostname, port = worker_list[worker_order[i]]
         x = i*per_worker
         y = min(end, (i+1)*per_worker)
-        assigned_workers.append((hostname, port, x, y))
-    return assigned_workers, worker_order[:n]
+        w = worker_order[i]
 
-def map_reduce(hash, n):
+        hostname, port = WORKERS[w]
+        new_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        new_sock.connect((hostname, port))
+        update_worker_power(q, w, penalty_new_worker)
+
+
+        assigned_workers.append((w, new_sock, x, y))
+    return assigned_workers
+
+def map_reduce(pool, q, hash, n):
     sockets = []
     processes = []
-    assigned_workers, worker_order = partition_job(WORKERS, 0, MAX_RANGE, n)
+    assigned_workers = partition_job(q, 0, MAX_RANGE, n)
     output = ""
-    pool = Pool()
     for i in range(n):
-        hostname, port, x, y = assigned_workers[i]
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((hostname, port))
-        processes.append(pool.apply_async(connect_worker, (worker_order[i], sock, x, y, hash)))
-        # decrease the power by 1 when assign the job to the worker
-        worker_power[worker_order[i]] = max(1, worker_power[worker_order[i]]-1)
+        w, sock, x, y = assigned_workers[i]
+        processes.append(pool.apply_async(connect_worker, (q, w, sock, x, y, hash)))
     while True:
         for i, f in enumerate(processes):
             if f.ready() and f.successful() and len(f.get()) == 5:
@@ -112,17 +159,17 @@ def map_reduce(hash, n):
                 break
         if output != "" and len(output) == 5:
             break
-    pool.close()
-    pool.terminate()
-    pool.join()
+    # pool.close()
+    # pool.terminate()
+    # pool.join()
     for sock in sockets:
         try:
             sock.sendall(str.encode("Stop\n"))
             sock.close()
-        except Exception as e:
+        except sock.error as e:
             print(e)
         finally:
-            socket.close()
+            sock.close()
     return output
 
 
@@ -136,5 +183,11 @@ if __name__ == "__main__":
     parser.add_argument('--num_workers', type=int, required=True)
 
     args = parser.parse_args()
-    output = map_reduce(args.hash, args.num_workers)
+    init()
+    pool = Pool()
+    manager = Manager()
+    q = manager.Queue()
+    watcher = pool.apply_async(listener, (q,))
+
+    output = map_reduce(pool, q, args.hash, args.num_workers)
     print(output)

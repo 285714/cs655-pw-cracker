57fd9139e7074cf7374681f7f6b922ab069c22df
Fabian Späh <fa-sph@web.de>
2021-12-04 11:57:09 -0500

    autodetect workers on server

--- src/server/workers.py
+++ src/server/workers.py
@@ -1,4 +1,24 @@
 # list of workers, each specified by a hostname + port
-WORKERS = [
-("csa2.bu.edu", 58800), ("csa2.bu.edu", 58801)
-]
+
+import os
+import re
+
+
+if True or os.environ['HOSTNAME'] == "server":
+    interfaces = os.popen("ifconfig").read().split("\n\n")
+    WORKERS = []
+    for interface in interfaces:
+        m = re.search("(eth[1-9]\d*):", interface)
+        if m is None: break
+        name = m[1]
+        m = re.search("inet (\S+)", interface)
+        ip = m[1]
+        WORKERS.append((ip, 58800))
+
+else:
+    WORKERS = [
+        ("localhost", 58800),
+        ("localhost", 58801)
+    ]
+
+

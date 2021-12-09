28b7988fb8f83f468a807d29a0c2a7630a34328d
Fabian Späh <fa-sph@web.de>
2021-12-08 19:18:00 -0500

    exp3

--- experiment/exp3.py
+++ experiment/exp3.py
@@ -12,42 +12,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os.path
+import scipy
+from scipy.optimize import curve_fit
 
-
-"""
-def measure_average_delay_single_request(start_index, num_trials, num_workers):
-    runtime = []
-    pwds = []
-    hashes = []
-    for i in range(start_index, start_index+num_trials):
-        pw = search.convert_order_to_string(i)
-        print(i, pw)
-        h = hashlib.md5(pw.encode()).hexdigest()
-        solve(h, num_workers,True)
-        print("done")
-        runtime.append(solved_hashes[h][1])
-        pwds.append(pw)
-        hashes.append(h)
-    return pwds, hashes, runtime
-
-def measure_average_delay_multiple_requests(start_index, num_requests, l): # rq arrives according to Poisson(l)
-# TODO
-    runtime = []
-    t = np.random.poisson(l,num_requests)
-    hashes = []
-    futures = []
-    for i in range(start_index, start_index+num_requests):
-        p = search.convert_order_to_string(i)
-        h = hashlib.md5(p.encode()).hexdigest()
-        pr = multiprocessing.Process(target=solve, args=(h,1))
-        futures.append(pr)
-        pr.start()
-        time.sleep(t[i-start_index])
-        print(i, t[i-start_index])
-    for pr in futures:
-        pr.join()
-    return solved_hashes
-"""
 
 csvfile = "exp3data.csv"
 
@@ -56,29 +23,27 @@ if not os.path.isfile(csvfile):
 
     df = pd.DataFrame(columns=["lam", "hash", "start_time", "time"])
 
-    start_time = time.time()
-    if __name__ == "__main__":
-        for lam in [2, 3, 4]:
-            for _ in range(10):
-                next_solve_time = np.random.poisson(lam=3)
-                time.sleep(next_solve_time)
+    for lam in [2]:
+        start_time = time.time()
+        for _ in range(10):
+            next_solve_time = np.random.poisson(lam=3)
+            time.sleep(next_solve_time)
 
-                ix = np.random.randint(0,MAX_RANGE)
-                pw = search.convert_order_to_string(ix)
-                # prefix = ["AA", "RA", "iA"][np.random.choice(3)]
-                prefix = ["AA", "aA"][np.random.choice(2)]
-                pw = prefix + pw[:3]
-                h = hashlib.md5(pw.encode()).hexdigest()
-                df = df.append({"lam":3, "hash": h, "start_time": time.time() - start_time, "time": -1},
-                        ignore_index=True)
-                solve(h, num_workers)
+            ix = np.random.randint(0,MAX_RANGE)
+            pw = search.convert_order_to_string(ix)
+            prefix = ["AB", "RB", "iB"][np.random.choice(3)]
+            # prefix = ["AD", "aD"][np.random.choice(2)]
+            pw = prefix + pw[:3]
+            h = hashlib.md5(pw.encode()).hexdigest()
+            df = df.append({"lam":lam, "hash": h, "start_time": time.time() - start_time, "time": -1},
+                    ignore_index=True)
+            solve(h, num_workers)
 
-                print("Beep")
-                print(df)
-                for h, (_, t) in solved_hashes.items():
-                    df.loc[df["hash"] == h, "time"] = t
-                # print(solved_hashes)
-        # time.sleep(5)
+            print(df)
+            for h, (_, t) in solved_hashes.items():
+                df.loc[df["hash"] == h, "time"] = t
+            # print(solved_hashes)
+    time.sleep(5)
 
     df = df.loc[df["time"] > 0]
     print(df)
@@ -86,20 +51,20 @@ if not os.path.isfile(csvfile):
 
 
 df = pd.read_csv(csvfile)
+fig = plt.figure(figsize=(8, 6))
 
-plt.scatter(df["start_time"], df["time"], c="red", alpha=0.5)
+for lam in pd.unique(df["lam"]):
+    print(lam)
+    dflam = df.loc[df["lam"] == lam]
+    x = dflam["start_time"]
+    y = dflam["time"]
+    def func(x, a, b, c):
+        return a * np.log(b * x) + c
+    plt.scatter(x, y, s=7, alpha=0.5)
+    popt, _ = curve_fit(func, x, y)
+    plt.plot(x, func(x, *popt), label="Po({})".format(lam))
+
+plt.legend()
 plt.show()
 
-
-"""
-    with open("average_time_pwd", 'w') as f:
-        f.write("num_workers,password,hash,runtime\n")
-    for i in range(10):
-        start_index = np.random.randint(0,MAX_RANGE)
-        print(i)
-        for num_workers in range(1,6):
-            pwds, hashes, runtime = measure_average_delay_single_request(start_index, 1, num_workers)
-            with open("average_time_pwd", 'a') as f:
-                f.write(str(num_workers) + ','+str(pwds[0]) + ',' +str(hashes[0]) + ',' +str(runtime[0]) + "\n")
-"""
 
